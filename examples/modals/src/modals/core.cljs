8b621855b923955a0d6617934d861cffec0c3a8a
Roberto Oliveros <robertooliveros@mac.com>
2014-12-26 12:52:55 -0600

    Modals with events implemented.

--- examples/modals/src/modals/core.cljs
+++ examples/modals/src/modals/core.cljs
@@ -1,8 +1,6 @@
 (ns modals.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :refer [chan put! pub sub]]
-            [jayq.core :refer [$]]
-            [full-control.core :as fc :refer-macros [defpage]]))
+  (:require [full-control.core :as fc :refer-macros [defpage]]
+            [full-control.events :as e]))
 
 (enable-console-print!)
 
@@ -11,37 +9,27 @@
 
 (defpage page [cursor owner opts]
   (init-state []
-              (let [modal-chan (chan)]
-                {:modal-chan modal-chan
-                 :modal-pub-chan (pub modal-chan :modal)}))
-  
+              {:modal-chs (e/init-chans)})
+
   (will-mount []
-              (let [c (chan)]
-                (sub (fc/get-state owner :modal-pub-chan) :modal c)
-                (go (while true
-                      (let [data (<! c)]
-                        (if (:show data)
-                          (-> ($ (str "#my-modal"))
-                              (.modal #js {:backdrop "static"}))
-                          (-> ($ (str "#my-modal"))
-                              (.modal "hide"))))))))
+              (e/listen :modal
+                        (fc/get-state owner [:modal-chs :pub])
+                        (e/modal-display "my-modal")))
   
   (render-state [st]
-                (fc/modal* {:id "my-modal"}
-                           (fc/modal-header* {}
-                                             (fc/h4* {} "My Modal"))
-                           (fc/p* {} "This is it!!")
-                           (fc/modal-footer* {}
-                                             (fc/button* {:on-click #(put! (:modal-chan st) {:modal :modal
-                                                                                             :show false})}
-                                                         "Close")))
+                (modal {:id "my-modal"}
+                       (header (title3 "My Modal"))
+                       (p "Hello Modal!")
+                       (footer (button {:on-click (e/emit (get-in st [:modal-chs :ch])
+                                                          (e/modal-hide :modal))}
+                                       "Close")))
                 (navbar (brand (:menu-h cursor)))
                 (fixed-layout
                  (row
                   (column-9
                    (panel (header (title3 (:panel-title cursor)))
-                          (button {:on-click #(put! (:modal-chan st) {:modal :modal
-                                                                      :show true})}
+                          (button {:on-click (e/emit (get-in st [:modal-chs :ch])
+                                                     (e/modal-show :modal))}
                                   "Open")))))))
 
 (fc/root page app-state {:target (. js/document (getElementById "app"))})
