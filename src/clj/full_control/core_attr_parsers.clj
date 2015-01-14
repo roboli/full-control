c227ac679266ffdb6d9cd4ebc7255cdf25e0921a
Roberto Oliveros <robertooliveros@mac.com>
2015-01-14 06:23:14 -0600

    Refactoring to use parse-group-for-attrs correctly.

--- src/clj/full_control/core_attr_parsers.clj
+++ src/clj/full_control/core_attr_parsers.clj
@@ -4,24 +4,25 @@
 ;;; Attributes parsers
 ;;;
 
-(defn- parse-m [[m & b :as body] & {:keys [not-found]}]
+(defn- parse-m [[m & b :as body] & {:keys [not-found]
+                                    :or {not-found [nil body]}}]
   (if (map? m)
     [m b]
-    (if (= (first m) 'with-attrs)
+    (if (and (seq? m) (= (first m) 'with-attrs))
       [(second m) (rest (rest m))]
-      [not-found body])))
+      not-found)))
 
 (defn- parse-attrs [body]
-  (parse-m body :not-found {}))
+  (parse-m body :not-found [{} body]))
 
 (defn- parse-layout-attrs [body]
-  (parse-m body :not-found {:column-size :md}))
+  (parse-m body :not-found [{:column-size :md} body]))
 
 (defn- parse-column-attrs [body]
   (let [[attrs body] (parse-attrs body)]
     [(assoc attrs :size (:column-size *attrs*)) body]))
 
-(defn- parse-group-for-attrs [[m & b]]
-  (if (map? m)
-    [m b]
-    [{:field-key m} b]))
+(defn- parse-group-for-attrs [[m & b :as body]]
+  (parse-m body :not-found (if (keyword? m)
+                             [{:field-key m} b]
+                             [{} body])))
