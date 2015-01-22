85bc95442ff523641a1fc35bf1d2ad4484a00aba
Roberto Oliveros <robertooliveros@mac.com>
2015-01-22 09:30:59 -0600

    Refactoring parsers.

--- src/clj/full_control/core_attr_parsers.clj
+++ src/clj/full_control/core_attr_parsers.clj
@@ -10,23 +10,37 @@
     [m b]
     (if (and (seq? m) (= (first m) 'with-attrs))
       [(second m) (rest (rest m))]
-      not-found)))
+      [nil body])))
+
+(defn- default-value [vl v]
+  (if-not (first v) (assoc v 0 vl) v))
+
+(def ^:private default-empty
+  (partial default-value {}))
+
+(def ^:private default-col-size
+  (partial default-value {:column-size :md}))
+
+(defn- merge-size [[m b]]
+  [(assoc m :size (or (:size m) (:column-size *attrs*))) b])
+
+(defn- merge-inline [[m b]]
+  [(assoc m :inline true) b])
+
+(defn- parse-field-key [[m & b :as body]]
+  (if (keyword? m) (cons {:field-key m} b) body))
 
 (defn- parse-attrs [body]
-  (parse-m body :not-found [{} body]))
+  ((comp default-empty parse-m) body))
 
 (defn- parse-layout-attrs [body]
-  (parse-m body :not-found [{:column-size :md} body]))
+  ((comp default-col-size parse-m) body))
 
 (defn- parse-column-attrs [body]
-  (let [[attrs body] (parse-attrs body)]
-    [(assoc attrs :size (or (:size attrs) (:column-size *attrs*))) body]))
+  ((comp merge-size default-empty parse-m) body))
 
-(defn- parse-group-for-attrs [[m & b :as body]]
-  (parse-m body :not-found (if (keyword? m)
-                             [{:field-key m} b]
-                             [{} body])))
+(defn- parse-group-for-attrs [body]
+  ((comp default-empty parse-m parse-field-key) body))
 
 (defn- parse-inline-attrs [body]
-  (let [[attrs body] (parse-attrs body)]
-    [(assoc attrs :inline true) body]))
+  ((comp merge-inline default-empty parse-m) body))
