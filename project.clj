643b3fdefe6cdd35bc92dc152047cf98bf3e8b8c
roboli <robertooliveros@mac.com>
2015-04-21 18:15:21 -0600

    om bumped to "0.8.8" and cljs to "0.0-2760".

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/roboli/full-control"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2760"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [om "0.7.3"]
+                 [org.omcljs/om "0.8.8"]
                  [camel-snake-kebab "0.2.5"]
                  [jayq "2.5.2"]]
 
@@ -18,56 +18,72 @@
                          :source-paths ["src/cljs" "examples/one-page/src"]
                          :compiler {:output-dir "examples/one-page/out"
                                     :output-to "examples/one-page/out/app.js"
-                                    :source-map "examples/one-page/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main one_page.core
                                     :optimizations :none}}
 
                         {:id "master-page"
                          :source-paths ["src/cljs" "examples/master-page/src"]
                          :compiler {:output-dir "examples/master-page/out"
                                     :output-to "examples/master-page/out/app.js"
-                                    :source-map "examples/master-page/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main master_page.core
                                     :optimizations :none}}
 
                         {:id "grids-tables"
                          :source-paths ["src/cljs" "examples/grids-tables/src"]
                          :compiler {:output-dir "examples/grids-tables/out"
                                     :output-to "examples/grids-tables/out/app.js"
-                                    :source-map "examples/grids-tables/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main grids_tables.core
                                     :optimizations :none}}
 
                         {:id "modals"
                          :source-paths ["src/cljs" "examples/modals/src"]
                          :compiler {:output-dir "examples/modals/out"
                                     :output-to "examples/modals/out/app.js"
-                                    :source-map "examples/modals/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main modals.core
                                     :optimizations :none}}
 
                         {:id "forms"
                          :source-paths ["src/cljs" "examples/forms/src"]
                          :compiler {:output-dir "examples/forms/out"
                                     :output-to "examples/forms/out/app.js"
-                                    :source-map "examples/forms/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main forms.core
                                     :optimizations :none}}
 
                         {:id "tabs"
                          :source-paths ["src/cljs" "examples/tabs/src"]
                          :compiler {:output-dir "examples/tabs/out"
                                     :output-to "examples/tabs/out/app.js"
-                                    :source-map "examples/tabs/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main tabs.core
                                     :optimizations :none}}
 
                         {:id "random"
                          :source-paths ["src/cljs" "examples/random/src"]
                          :compiler {:output-dir "examples/random/out"
                                     :output-to "examples/random/out/app.js"
-                                    :source-map "examples/random/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main random.core
                                     :optimizations :none}}
 
                         {:id "pagers"
                          :source-paths ["src/cljs" "examples/pagers/src"]
                          :compiler {:output-dir "examples/pagers/out"
                                     :output-to "examples/pagers/out/app.js"
-                                    :source-map "examples/pagers/out/app.js.map"
+                                    :source-map true
+                                    :asset-path "out"
+                                    :main pagers.core
                                     :optimizations :none}}]}
 
   :clean-targets ["examples/one-page/out"
